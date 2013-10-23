a7442f8486203434488c88b6d7bfdede83553f69
Andy Lindeman <andy@andylindeman.com>
2013-10-23 18:56:12 -0400

    RegisterSet can be merged with other sets

--- lib/hyperll/register_set.rb
+++ lib/hyperll/register_set.rb
@@ -1,8 +1,12 @@
 module Hyperll
   class RegisterSet
+    include Enumerable
+
     LOG2_BITS_PER_WORD = 6
     REGISTER_SIZE = 5
     INTEGER_SIZE = 32
+
+    attr_reader :count, :values
 
     def initialize(count)
       @count = count
@@ -31,6 +35,43 @@ module Hyperll
       shift = REGISTER_SIZE * (position - (bucket * LOG2_BITS_PER_WORD))
 
       return (@values[bucket] & (0x1f << shift)) >> shift
+    end
+
+    def each
+      return enum_for(:each) unless block_given?
+      @size.times do |i|
+        yield self[i]
+      end
+    end
+
+    def update_if_greater(position, value)
+      bucket = position / LOG2_BITS_PER_WORD
+      shift = REGISTER_SIZE * (position - (bucket * LOG2_BITS_PER_WORD));
+      mask = 0x1f << shift;
+
+      current_value = @values[bucket] & mask
+      new_value = value << shift
+      if current_value < new_value
+        @values[bucket] = (@values[bucket] & ~mask) | new_value
+        true
+      else
+        false
+      end
+    end
+
+    def merge(other)
+      @size.times do |bucket|
+        word = 0
+        LOG2_BITS_PER_WORD.times do |j|
+          mask = 0x1f << (REGISTER_SIZE * j);
+
+          this_val = self.values[bucket] & mask
+          other_val = other.values[bucket] & mask
+          word |= [this_val, other_val].max
+        end
+
+        @values[bucket] = word
+      end
     end
   end
 end
--- spec/hyperll/register_set_spec.rb
+++ spec/hyperll/register_set_spec.rb
@@ -15,5 +15,57 @@ module Hyperll
       rs[0] = 11
       expect(rs[0]).to eq(11)
     end
+
+    it "merges with other register sets" do
+      rand = Random.new(2)
+      count = 32
+      rs = RegisterSet.new(count)
+
+      rss = []
+      5.times do |i|
+        rss[i] = RegisterSet.new(count)
+        count.times do |pos|
+          val = rand.rand(10)
+          rs.update_if_greater(pos, val)
+          rss[i][pos] = val
+        end
+      end
+
+      merged = RegisterSet.new(count)
+      rss.each do |set|
+        merged.merge(set)
+      end
+
+      rs.each_with_index do |value, index|
+        expect(value).to eq(merged[index])
+      end
+    end
+
+    it "merges with other register sets using update_if_greater" do
+      rand = Random.new(2)
+      count = 32
+      rs = RegisterSet.new(count)
+
+      rss = []
+      5.times do |i|
+        rss[i] = RegisterSet.new(count)
+        count.times do |pos|
+          val = rand.rand(10)
+          rs.update_if_greater(pos, val)
+          rss[i][pos] = val
+        end
+      end
+
+      merged = RegisterSet.new(count)
+      rss.each do |set|
+        set.each_with_index do |value, index|
+          merged.update_if_greater(index, value)
+        end
+      end
+
+      rs.each_with_index do |value, index|
+        expect(value).to eq(merged[index])
+      end
+    end
   end
 end
