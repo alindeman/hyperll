ef08ab1a5a3eebe818711c25db5eecd00322d825
Andy Lindeman <andy@andylindeman.com>
2013-10-29 12:43:56 -0400

    Adds second performance test

---
+++ spec/plus_merge_performance2.rb
@@ -0,0 +1,32 @@
+require 'json'
+require 'base64'
+require 'hyperll'
+require 'rblineprof'
+
+fixtures = JSON.load(File.read(File.join(File.dirname(__FILE__), 'fixtures', 'mega.json')))
+hllps = fixtures.compact.map { |line|
+  Hyperll::HyperLogLogPlus.unserialize(Base64.decode64(line))
+}
+
+hllp = Hyperll::HyperLogLogPlus.new(11, 16)
+profile = lineprof(/hyper_log_log_plus\.rb/) do
+  hllps.each do |h|
+    hllp.merge(h)
+  end
+end
+
+puts "Total cardinality: #{hllp.cardinality}"
+
+profile.each do |file, lines|
+  File.readlines(file).each_with_index do |line, num|
+    wall, cpu, calls, allocations, *rest = lines[num+1]
+
+    if calls && calls > 0
+      printf "% 8.1fms + % 8.1fms (% 8d) | %s", cpu/1000.0, (wall-cpu)/1000.0, calls, line
+      # printf "% 8.1fms (% 5d) | %s", wall/1000.0, calls, line
+    else
+      printf "                                   | %s", line
+      # printf " | %s", line
+    end
+  end
+end
